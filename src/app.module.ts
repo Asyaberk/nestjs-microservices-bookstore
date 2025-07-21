aeab49587ad7626a964bdad5dc90e641ca0ad9c2
Asyaberk <assya.berk@gmail.com>
2025-07-21 12:43:03 +0300

    chore: configured PostgreSQL connection with TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    database: 'dbpostgre',
+    host: 'localhost',
+    port: 5432,
+    username: 'asya',
+    password: 'Asya1234',
+    autoLoadEntities: true,
+    synchronize: true, 
+  }), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
