0ddde483faba4f340e5022d14120366b062d7a32
Asyaberk <assya.berk@gmail.com>
2025-08-14 00:18:52 +0300

    feat(cache): practice auto caching with global config and method-level overrides

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,17 +11,10 @@ import { BookModule } from './books/book.module';
 import { Book } from './books/entities/books.entity';
 import { LibraryModule } from './library/library.module';
 import { Rental } from './library/entities/rental.entity';
-import { CacheModule } from '@nestjs/cache-manager';
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
 import { ConfigModule } from '@nestjs/config'; 
-
-/*
-REDIS SETTING UP
-docker run --name redis -p 6379:6379 -d redis 
-docker exec -it redis redis-cli
-ping pong ok
-SET myKey "hello redis"
-GET myKey
-*/
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { AppController2 } from './app.controller2';
 
 @Module({
   imports: [
@@ -33,13 +26,19 @@ GET myKey
       port: Number(process.env.DB_PORT ?? 5432),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
-      host: process.env.DB_HOST,
-      synchronize: true,
+      //i commented this docker db conneciton because 
+      //i am using npm run start:dev 
+      //just for now to compile faster and reduce storage(i do not have enough)
+      //it needs its local db host name which is localhost
+      //host: process.env.DB_HOST,
+      host: 'localhost',
+      synchronize: true, 
       database: process.env.DB_DATABASE,
       entities: [User, Role, Book, Rental],
     }),
 
-    //WORKING WITH CACHE STORE
+    //WORKING WITH CACHE STORE(global caching)
+    //global defauşt config
     CacheModule.register({
       //in seconds
       ttl: 10_000,
@@ -52,7 +51,18 @@ GET myKey
     BookModule,
     LibraryModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [
+    AppController,
+    //for demo 
+    AppController2
+  ],
+  providers: [
+    AppService,
+    //AUTO CACHING RESPONSE
+    // {
+    //   provide: APP_INTERCEPTOR,
+    //   useClass: CacheInterceptor,
+    // },
+  ],
 })
 export class AppModule {}
